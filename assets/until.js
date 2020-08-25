7b25780791dd7ab9080599e44fde10fb91e5ed1d
gamezxii <39672948+gamezxii@users.noreply.github.com>
2020-08-25 14:36:30 +0700

    Update until.js

--- assets/until.js
+++ assets/until.js
@@ -1,12 +1,12 @@
 export const getAPIX =
-  'https://fountaintreeresort.com/game/Webapi/?op=getActivitynameX';
+  'https://....com/game/Webapi/?op=getActivitynameX';
 export const getApiY =
-  'https://fountaintreeresort.com/game/Webapi/?op=getActivitynameY';
+  'https://....com/game/Webapi/?op=getActivitynameY';
 export const getAPIZ =
-  'https://fountaintreeresort.com/game/Webapi/?op=getActivitynameZ';
+  'https://....com/game/Webapi/?op=getActivitynameZ';
 
-export const ApiScanY = 'https://fountaintreeresort.com/game/Webapi/?op=Y';
+export const ApiScanY = 'https://....com/game/Webapi/?op=Y';
 
-export const ApiScan = 'https://fountaintreeresort.com/game/Webapi/?op=';
+export const ApiScan = 'https://....com/game/Webapi/?op=';
 
-export const Model = 'https://fountaintreeresort.com/game/cashcard/Model/index.php';
+export const Model = 'https://....com/game/cashcard/Model/index.php';
